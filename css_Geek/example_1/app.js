06c44c6e0db0a1642dd19942b107fab7a9b97e84
dhamija.shashank <dhamija.shashank@gmail.com>
2017-11-16 16:18:40 +0530

    Example for video 1 & 2

---
+++ css_Geek/example_1/app.js
@@ -0,0 +1,26 @@
+
+//video 1 & 2 installation and basic
+
+function add(){
+	// return a+b;
+	console.log("In Add method");
+}
+
+console.log(add());
+
+var obj = {
+	name: "krishna",
+	age: '222'
+}
+
+console.log(obj)
+console.log("Name is: "+obj.name)
+console.log("Age is "+obj.age)
+
+function add(a,b){
+	
+	console.log("In Add method");
+	return a+b;
+}
+
+console.log(add(5,6));
---
+++ css_Geek/example_1/app2.js
@@ -0,0 +1,10 @@
+
+
+var print = function (){
+	console.log("Hello friends in Annonomus function without name");
+}
+
+print();
+
+console.log("Below output is shown after 8 second")
+setTimeout(print,8000);
