fd2289370200609daa43fbcac608daddef51393f
dhamija.shashank <dhamija.shashank@gmail.com>
2017-11-16 18:03:23 +0530

    add code for video 5

--- css_Geek/example_3/app.js
+++ css_Geek/example_3/app.js
@@ -9,3 +9,15 @@ var annonomus_name = {
 
 annonomus_name.printName();
 
+
+function worth_less(){
+	console.log(this == global)
+}
+
+
+worth_less();
+
+//output is true for line number 6 as the object which is present in annonomus
+// function is of type annonomus_name class so it return true
+
+//output for line number 14 is also true as it call with global object 
